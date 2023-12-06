fae8fe8d4813cf879a84016bfc3cfc77a6c44228
Jack Manhardt <jackmanhardt@gmail.com>
2023-12-06 10:53:52 -0600

    documentation

--- MyHeap.h
+++ MyHeap.h
@@ -41,12 +41,15 @@ public:
   MyHeap &operator=(const MyHeap &source);
 
   // Returns a reference to the highest priority element
+  // Should throw an out_of_range exception if the heap is empty
   T &top() const;
 
   // Removes the highest priority element, and restores heap property
+  // should throw out_of_range exception if heap is empty
   void pop();
 
   // Adds `val` to the heap, and restores heap property
+  // Special case needed for empty heap (m_data = nullptr)
   void push(const T &val);
 
   // True if heap is empty, false otherwise
